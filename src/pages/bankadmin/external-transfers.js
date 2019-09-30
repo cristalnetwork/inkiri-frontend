6f6952e0271f79cc283ab51db6e131b22763d580
tuti <ptutino@gmail.com>
2019-09-30 17:48:24 -0300

    Added missing files from last commit

---
+++ src/pages/bankadmin/external-transfers.js
@@ -0,0 +1,377 @@
+import React, {useState, Component} from 'react'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
+
+// import * as userRedux from '@app/redux/models/user';
+import * as loginRedux from '@app/redux/models/login'
+
+import * as globalCfg from '@app/configs/global';
+
+import * as api from '@app/services/inkiriApi';
+import * as routesService from '@app/services/routes';
+
+import { Route, Redirect, withRouter } from "react-router-dom";
+
+import { Radio, Select, Card, PageHeader, Tag, Tabs, Button, Statistic, Row, Col, List } from 'antd';
+import { Form, Input, Icon} from 'antd';
+import { notification, Table, Divider, Spin } from 'antd';
+
+import './pda.css'; 
+import styles from './style.less'; 
+
+import {columns,  DISPLAY_ALL_TXS, DISPLAY_PROVIDER, DISPLAY_EXCHANGES} from '@app/components/TransactionTable';
+
+import * as utils from '@app/utils/utils';
+
+const { TabPane } = Tabs;
+const FormItem = Form.Item;
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+const { Option } = Select;
+const { Search, TextArea } = Input;
+
+const routes = routesService.breadcrumbForFile('external-transfers');
+
+//
+
+class ExternalTransfers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading:        false,
+      txs:            [],
+      
+      page:           -1, 
+      limit:           globalCfg.api.default_page_size,
+      can_get_more:    true,
+
+      stats:          {},
+      active_tab:     DISPLAY_ALL_TXS
+    };
+
+    this.loadTransactionsForPDA     = this.loadTransactionsForPDA.bind(this);  
+    this.openNotificationWithIcon   = this.openNotificationWithIcon.bind(this); 
+    this.renderFooter               = this.renderFooter.bind(this); 
+    this.onNewData                  = this.onNewData.bind(this);
+    // this.onTabChange                = this.onTabChange.bind(this);
+    this.onTableChange              = this.onTableChange.bind(this);
+    this.onProcessRequestClick      = this.onProcessRequestClick.bind(this);
+  }
+  
+  componentDidMount(){
+    this.loadTransactionsForPDA();  
+  } 
+
+  loadTransactionsForPDA(){
+
+    let can_get_more   = this.state.can_get_more;
+    if(!can_get_more)
+    {
+      this.setState({loading:false});
+      return;
+    }
+
+    
+    this.setState({loading:true});
+
+    let page           = (this.state.page<0)?0:(this.state.page+1);
+    const limit          = this.state.limit;
+    let that           = this;
+    
+    const req_type = DISPLAY_EXCHANGES + '|' + DISPLAY_PROVIDER;
+    const account_name = undefined;
+    
+    api.bank.listRequests(page, limit, req_type, account_name)
+    .then( (res) => {
+        that.onNewData(res);
+      } ,(ex) => {
+        // console.log('---- ERROR:', JSON.stringify(ex));
+        that.setState({loading:false});  
+      } 
+    );
+    
+  }
+
+  onNewData(txs){
+    
+    const _txs            = [...this.state.txs, ...txs];
+    const pagination      = {...this.state.pagination};
+    pagination.pageSize   = _txs.length;
+    pagination.total      = _txs.length;
+
+    const has_received_new_data = (txs && txs.length>0);
+
+    this.setState({pagination:pagination, txs:_txs, can_get_more:(has_received_new_data && txs.length==this.state.limit), loading:false})
+
+    if(!has_received_new_data)
+    {
+      this.openNotificationWithIcon("info", "End of transactions","You have reached the end of transaction list!")
+    }
+    else
+      this.computeStats();
+  }
+
+  
+  computeStats(txs){
+    let stats = this.currentStats();
+    if(txs===undefined)
+      txs = this.state.txs;
+    const deposits      = txs.filter( tx => globalCfg.api.isDeposit(tx))
+                    .map(tx =>tx.quantity)
+                    .reduce((acc, amount) => acc + Number(amount), 0);
+    const withdraws     = txs.filter( tx => globalCfg.api.isWithdraw(tx))
+                    .map(tx =>tx.quantity)
+                    .reduce((acc, amount) => acc + Number(amount), 0);
+    
+    const deposits_ik   = txs.filter( tx => globalCfg.api.isIKDeposit(tx))
+                    .map(tx =>tx.quantity)
+                    .reduce((acc, amount) => acc + Number(amount), 0);
+    const deposits_brl  = txs.filter( tx => globalCfg.api.isBRLDeposit(tx))
+                    .map(tx =>tx.quantity)
+                    .reduce((acc, amount) => acc + Number(amount), 0);
+    const pending      = txs.filter( tx => globalCfg.api.isProcessPending(tx))
+                    .map(tx =>tx.quantity).length;
+
+    stats[this.state.active_tab] = {
+        withdraws:     withdraws
+        , deposits:    deposits
+        , count:       txs.length
+        , deposits_ik: deposits_ik 
+        , deposits_brl:deposits_brl 
+        , pending:     pending};
+
+    this.setState({stats:stats})
+  }
+
+  currentStats(){
+    const x = this.state.stats[this.state.active_tab];
+    const _default = {deposits:0 
+              , withdraws:0
+              , count:0
+              , deposits_ik:0
+              , deposits_brl:0
+              , pending:0};
+    return x?x:_default;
+  }
+
+  openNotificationWithIcon(type, title, message) {
+    notification[type]({
+      message: title,
+      description:message,
+    });
+  }
+  // Component Events
+  
+  onTableChange(key, txs) {
+    // console.log(key);
+    // this.setState({active_tab:key})
+    if(key==this.state.active_tab )
+      this.computeStats(txs);
+  }
+
+  onProcessRequestClick(request){
+    console.log( ' PDA::onProcessRequestClick >> ', JSON.stringify(request) )
+
+    this.props.history.push({
+      pathname: `/${this.props.actualRole}/external-transfers-process-request`
+      // , search: '?query=abc'
+      , state: { request: request }
+    })
+
+    // this.props.history.push({
+    //   pathname: '/template',
+    //   search: '?query=abc',
+    //   state: { detail: response.data }
+    // })
+    // READ >> this.props.location.state.detail
+  }
+
+  renderFooter(){
+    return (<><Button key="load-more-data" disabled={!this.state.can_get_more} onClick={()=>this.loadTransactionsForPDA()}>More!!</Button> </>)
+  }
+
+  //
+  renderSelectTxTypeOptions(){
+    return (
+      globalCfg.api.getTypes().map( tx_type => {return(<Option key={'option'+tx_type} value={tx_type} label={utils.firsts(tx_type.split('_')[1])}>{ utils.capitalize(tx_type.split('_')[1]) } </Option>)})
+        )
+  }
+  // 
+  renderSelectTxStateOptions(){
+    return (
+      globalCfg.api.getStates().map( tx_state => {return(<Option key={'option'+tx_state} value={tx_state} label={utils.firsts(tx_state.split('_')[1])}>{ utils.capitalize(tx_state.split('_')[1]) } </Option>)})
+        )
+  }
+  //
+  renderFilterContent() {
+    return (
+      <div className="wrap">
+        <Row>
+          <Col span={24}>
+            <Form layout="inline" className="filter_form" onSubmit={this.handleSubmit}>
+              <Form.Item label="Transaction type">
+                  <Select placeholder="Transaction type"
+                    mode="multiple"
+                    style={{ minWidth: '250px' }}
+                    defaultValue={['ALL']}
+                    optionLabelProp="label">
+                      {this.renderSelectTxTypeOptions()}
+                  </Select>
+              </Form.Item>
+              <Form.Item label="Transaction status">
+                <Select placeholder="Transaction status"
+                    mode="multiple"
+                    style={{ minWidth: '250px' }}
+                    defaultValue={['ALL']}
+                    optionLabelProp="label">
+                      {this.renderSelectTxStateOptions()}
+                  </Select>
+              </Form.Item>
+              <Form.Item label="Search">
+                  <Search className="styles extraContentSearch" placeholder="Search" onSearch={() => ({})} />
+              </Form.Item>
+              <Form.Item>
+                <Button htmlType="submit" disabled>
+                  Filter
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+
+  
+  render() {
+    //
+    const filters = this.renderFilterContent();
+    const content = this.renderUMIContent();
+    return (
+      <>
+        <PageHeader
+          extra={[
+            <Button key="_new_deposit"  icon="plus" disabled> Deposit</Button>,
+            <Button key="_new_withdraw" icon="plus" disabled> Withdraw</Button>,
+          ]}
+          breadcrumb={{ routes }}
+          title="External Transfers"
+          subTitle="List of Provider Payments and Exchanges"
+        >
+          
+        </PageHeader>
+        {filters}
+        {content}
+
+      </>
+    );
+  }
+//
+  renderExtraContent (){ 
+    
+    return(
+      <div className="styles extraContent" style={{display:'none'}}>
+        <RadioGroup defaultValue="all">
+          <RadioButton value="all">all</RadioButton>
+          <RadioButton value="progress">progress</RadioButton>
+          <RadioButton value="waiting">waiting</RadioButton>
+        </RadioGroup>&nbsp;
+        <Search className="styles extraContentSearch" placeholder="Search" onSearch={() => ({})} />
+      </div>
+    )};
+
+    //
+  
+  renderUMIContent(){
+    const {exchanges, exchanges_pending, providers, providers_pending, total_out} = this.currentStats();  
+    return  (<>
+      <div className="styles standardList" style={{ marginTop: 24 }}>
+        <Card key="the_card_key" bordered={false}>
+          <Row>
+            <Col xs={24} sm={12} md={6} lg={4} xl={4}>
+              <Statistic title="" value="STATS" />
+            </Col>
+            <Col xs={24} sm={12} md={6} lg={4} xl={4}>
+              <Statistic
+                title={"EXCHANGES"}
+                value={exchanges}
+                precision={2}
+                suffix={globalCfg.currency.symbol}
+              />
+            </Col>
+            <Col xs={24} sm={12} md={6} lg={4} xl={4}>
+              <Statistic
+                title={"EXCHANGES PENDING"}
+                value={exchanges_pending}
+                precision={2}
+                valueStyle={{ color: '#fadb14' }}
+                prefix={<Icon type="clock-circle" />}
+              />
+            </Col>
+            <Col xs={24} sm={12} md={6} lg={4} xl={4}>
+              <Statistic
+                title="PROVIDERS PAY"
+                value={providers}
+                precision={2}
+                suffix={globalCfg.currency.symbol}
+              />
+            </Col>
+            <Col xs={24} sm={12} md={6} lg={4} xl={4}>
+              <Statistic
+                title="PROVIDERS PAY PENDING"
+                value={providers_pending}
+                precision={2}
+                valueStyle={{ color: '#fadb14' }}
+                prefix={<Icon type="clock-circle" />}
+              />
+            </Col>
+            <Col xs={24} sm={12} md={6} lg={4} xl={4}>
+              <Statistic
+                title="TOTAL OUT"
+                value={total_out}
+                precision={0}
+                valueStyle={{ color: '#cf1322' }}
+                suffix={globalCfg.currency.symbol}
+              />
+            </Col>
+          </Row>
+        </Card>
+
+        <Card
+          key="card_table_all_requests"
+          className="styles listCard"
+          bordered={false}
+          title="List of Provider Payments and Exchanges"
+          style={{ marginTop: 24 }}
+          extra={this.renderExtraContent()}
+        >
+          <Table
+            key="table_all_requests" 
+            rowKey={record => record.id} 
+            loading={this.state.loading} 
+            columns={columns(this.props.actualRoleId, this.onProcessRequestClick)} 
+            dataSource={this.state.txs} 
+            footer={() => this.renderFooter()}
+            pagination={this.state.pagination}
+            scroll={{ x: 700 }}
+            />
+
+        </Card>
+      </div>
+    </>)
+  }
+
+}
+//
+export default  (withRouter(connect(
+    (state)=> ({
+        actualAccountName:    loginRedux.actualAccountName(state),
+        actualRoleId:     loginRedux.actualRoleId(state),
+        actualRole:       loginRedux.actualRole(state),
+    }),
+    (dispatch)=>({
+        // tryUserState: bindActionCreators(userRedux.tryUserState , dispatch)
+    })
+)(ExternalTransfers))
+);
---
+++ src/pages/bankadmin/processExternal.js
@@ -0,0 +1,360 @@
+import React, {useState, Component} from 'react'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
+
+import * as loginRedux from '@app/redux/models/login'
+import * as accountsRedux from '@app/redux/models/accounts'
+import * as balanceRedux from '@app/redux/models/balance'
+
+import * as api from '@app/services/inkiriApi';
+import * as routesService from '@app/services/routes';
+import * as globalCfg from '@app/configs/global';
+import * as utils from '@app/utils/utils';
+
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+
+import { Result, Card, PageHeader, Tag, Button, Statistic, Row, Col, Spin, Descriptions } from 'antd';
+import { notification, Form, Icon, InputNumber, Input, AutoComplete, Typography } from 'antd';
+
+import './processRequest.css'; 
+import styles from './processRequest.less';
+
+const { Paragraph, Text } = Typography;
+
+
+const Description = ({ term, children, span = 12 }) => (
+    <Col span={span}>
+      <div className="description">
+        <div className="term">{term}</div>
+        <div className="detail">{children}</div>
+      </div>
+    </Col>
+  );
+
+const routes = routesService.breadcrumbForFile('external-transfers');
+
+class processExternal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading:      false,
+      dataSource:   [],
+      receipt:      '',
+      amount:       0,
+      memo:         '',
+      pushingTx:    false,
+      result:       undefined,
+      result_object:undefined,
+      error:        {},
+      number_validateStatus : '',
+      number_help:  '',
+       
+      request:{} 
+    
+    };
+
+    // this.handleSearch = this.handleSearch.bind(this); 
+    this.onSelect                   = this.onSelect.bind(this); 
+    this.renderContent              = this.renderContent.bind(this); 
+    this.handleSubmit               = this.handleSubmit.bind(this);
+    this.onChange                   = this.onChange.bind(this); 
+    this.resetPage                  = this.resetPage.bind(this); 
+    this.openNotificationWithIcon   = this.openNotificationWithIcon.bind(this); 
+    this.renderInfoForRequest       = this.renderInfoForRequest.bind(this);
+    this.renderInfoForDeposit       = this.renderInfoForDeposit.bind(this);
+    this.issueMoney                 = this.issueMoney.bind(this);
+  }
+
+  static propTypes = {
+    match: PropTypes.object,
+    location: PropTypes.object,
+    history: PropTypes.object
+  };
+
+  componentDidMount(){
+    const { match, location, history } = this.props;
+    // console.log( 'processRequest::router-params >>' , JSON.stringify(this.props.location.state.request) );
+    if(this.props.location && this.props.location.state)
+      this.setState({request : this.props.location.state.request})
+  }
+  
+  onSelect(value) {
+    console.log('onSelect', value);
+    this.setState({receipt:value})
+  }
+
+  onChange(e) {
+    e.preventDefault();
+    // console.log('changed', e);
+    this.setState({amount:e.target.value, number_validateStatus:'' , number_help:''})
+  }
+
+  // onSearch={this.handleSearch}
+  handleSearch(value){
+    // this.setState({
+    //   dataSource: !value ? [] : [value, value + value, value + value + value],
+    // });
+  };
+
+  openNotificationWithIcon(type, title, message) {
+    notification[type]({
+      message: title,
+      description:message,
+    });
+  }
+
+  issueMoney = async () => {
+    
+    // return;
+    const {_id, amount, requested_by, requestCounterId, deposit_currency} = this.state.request;
+    const privateKey = this.props.actualPrivateKey;
+    const receiver   = requested_by.account_name;
+    const sender     = globalCfg.currency.issuer; //this.props.actualAccountName;
+    
+    const fiat       = globalCfg.api.fiatSymbolToMemo(deposit_currency)
+    const memo       = `dep|${fiat}|${requestCounterId.toString()}`;
+
+    let that = this;
+    that.setState({pushingTx:true});
+    api.issueMoney(sender, privateKey, receiver, amount, memo)
+      .then(data => {
+        console.log(' processRequest::issue (then#1) >>  ', JSON.stringify(data));
+        if(data && data.data && data.data.transaction_id)
+        {
+          // updeteo la tx
+          api.bank.setDepositOk(_id, data.data.transaction_id).then(
+            // muestro resultado
+            (update_res) => {
+              console.log(' processRequest::issue (then#2) >> update_res ', JSON.stringify(update_res), JSON.stringify(data));
+              console.log(' processRequest::issue (then#2) >> data ', JSON.stringify(data));
+              that.setState({result:'ok', pushingTx:false, result_object:data});
+            }, (err) => {
+              that.setState({result:'error', pushingTx:false, error:JSON.stringify(err)});
+            }
+          )
+        }
+        else{
+          that.setState({result:'error', pushingTx:false, error:'UNKNOWN!'});
+        }
+        
+      }, (ex)=>{
+        console.log(' processRequest::issue (error#1) >>  ', JSON.stringify(ex));
+        that.setState({result:'error', pushingTx:false, error:JSON.stringify(ex)});
+      });
+  }
+
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.issueMoney();
+  };
+
+  backToDashboard = async () => {
+    this.props.history.push({
+      pathname: `/${this.props.actualRole}/dashboard`
+    })
+  }
+
+  backToReferrer = async () => {
+    this.props.history.push({
+      pathname: `/${this.props.actualRole}/external-transfers`
+    })
+  }
+
+  resetPage(){
+    this.setState({result: undefined, result_object: undefined, error: {}});
+  }
+
+  renderContent() {
+  
+    const { getFieldDecorator } = this.props.form;
+    
+    if(this.state.result=='ok')
+    {
+      const tx_id = api.dfuse.getTxId(this.state.result_object?this.state.result_object.data:{});
+      const _href = api.dfuse.getBlockExplorerTxLink(tx_id);
+      // console.log(' >>>>> api.dfuse.getBlockExplorerTxLink: ', _href)
+      
+      return (<Result
+        status="success"
+        title="Transaction completed successfully!"
+        subTitle={`Transaction id ${tx_id}. Cloud server takes up to 30 seconds, please wait.`}
+        extra={[
+          <Button type="primary" key="go-to-dashboard" onClick={()=>this.backToDashboard()}>
+            Go to dashboard
+          </Button>,
+          <Button  key="go-to-pda" onClick={()=>this.backToReferrer()}>
+            Back to External Transfers
+          </Button>,
+          <Button type="link" href={_href} target="_blank" key="view-on-blockchain" icon="cloud" >View on Blockchain</Button>,
+          
+        ]}
+      />)
+    }
+    //`
+    if(this.state.result=='error')
+    {
+
+      // <Button key="re-send">Try sending again</Button>,
+      return (<Result
+                status="error"
+                title="Transaction Failed"
+                subTitle="Please check and modify the following information before resubmitting."
+                extra={[
+                  <Button type="primary" key="go-to-dashboard" onClick={()=>this.backToDashboard()}>Go to dashboard</Button>,
+                  <Button shape="circle" icon="close-circle" key="close" onClick={()=>this.resetPage()} />
+                ]}
+              >
+                <div className="desc">
+                  <Paragraph>
+                    <Text
+                      strong
+                      style={{ fontSize: 16, }}
+                    >
+                      The content you submitted has the following error:
+                    </Text>
+                  </Paragraph>
+                  <Paragraph>
+                    <Icon style={{ color: 'red' }} type="close-circle" /> {this.state.error}
+                  </Paragraph>
+                </div>
+              </Result>)
+    }
+    
+    // ** hack for sublime renderer ** //
+
+    const {amount, requested_by} = this.state.request;
+    const cant_process           = globalCfg.api.isFinished(this.state.request)
+    return (
+        <div style={{ margin: '0 auto', width:500, padding: 24, background: '#fff'}}>
+          <Spin spinning={this.state.pushingTx} delay={500} tip="Pushing transaction...">
+            <Form onSubmit={this.handleSubmit}>
+                
+              
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button" disabled={cant_process}>
+                  Accept Deposit & Issue
+                </Button>
+                
+              </Form.Item>
+            </Form>
+          </Spin>
+        </div>
+    );
+  }
+  //
+  renderInfoForRequest(){
+    switch(this.state.request.requested_type) {
+      case globalCfg.api.TYPE_DEPOSIT:
+        return this.renderInfoForDeposit() 
+        break;
+      case globalCfg.api.TYPE_WITHDRAW:
+        return this.renderInfoForWithdraw() 
+        break;
+      default:
+        return (<></>);
+    }
+  }
+  // ** hack for sublime renderer ** //
+  renderInfoForDeposit() 
+  {
+    const {tx_id, deposit_currency, requested_type, amount, quantity_txt, state, created_at, requestCounterId, subheader, requested_by} = this.state.request;
+    const envelope_id = api.bank.envelopeIdFromRequest(this.state.request);
+    const _requested_type = requested_type.split('_')[1].toUpperCase();
+    const _state          = state.split('_')[1].toUpperCase();
+    let viewOnBlockchain = (<></>);
+    if(tx_id)
+      {
+        const _href = api.dfuse.getBlockExplorerTxLink(tx_id);
+        viewOnBlockchain = (<Button type="link" href={_href} target="_blank" key="view-on-blockchain" icon="cloud" >View on Blockchain</Button>)
+      }
+    return (<Descriptions className="headerList" size="small" column={2}>
+      <Descriptions.Item  label = " Requested By " > {requested_by.account_name} ({requested_by.email})</ Descriptions.Item >
+      <Descriptions.Item  label = " Request type " > <Tag color="#108ee9">{_requested_type}</Tag> </ Descriptions.Item >
+      <Descriptions.Item  label = " Created at " >{created_at}</ Descriptions.Item >
+      <Descriptions.Item  label = " Amount " >{quantity_txt}</ Descriptions.Item >
+      <Descriptions.Item  label = " Associated Documents " >
+        <a href="#"><i>nothing yet</i></a>
+      </Descriptions.Item>
+      <Descriptions.Item  label = " Envelope Id " >{envelope_id}</ Descriptions.Item >
+      <Descriptions.Item  label = " Status " >{_state}</ Descriptions.Item >
+      <Descriptions.Item  label = " Blockchain Link" >
+        {viewOnBlockchain}
+      </ Descriptions.Item >
+    </Descriptions>);
+
+    // const current_stats = this.currentStats();
+    // return (
+    //   <Row>
+    //     <Description term="Entradas"><Tag color="green">IK$ {current_stats.money_in.toFixed(2)}</Tag></Description>
+    //     <Description term="Variacao de caja"><Tag color="red">IK$ {(current_stats.money_in - current_stats.money_out).toFixed(2)}</Tag></Description>
+    //     <Description term="Saidas"><Tag color="red">-IK$ {current_stats.money_out.toFixed(2)}</Tag></Description>
+    //     <Description term="Lançamentos">{current_stats.count|0}</Description>
+    //   </Row>
+    // );
+  }  
+  // ** hack for sublime renderer ** //
+
+  renderExtraContent ()
+  {
+    return(
+      <Row>
+        
+        <Col xs={24} sm={24}>
+          <div className ="textSecondary" > Customer Balance ({globalCfg.currency.symbol}) </div>
+          <div className ="heading" >{Number(0).toFixed(2)}</div>
+        </Col>
+      </Row>
+    );
+  }
+  
+  //
+  render() {
+    let content = this.renderContent();
+    
+    return (
+      <>
+        <PageHeader
+          breadcrumb={{ routes }}
+          title="Process Request"
+          subTitle="Process customer request"
+          
+        >
+         
+         <div className="wrap">
+            <div className="content padding">{this.renderInfoForRequest()}</div>
+            <div className="extraContent">{this.renderExtraContent()}</div>
+          </div>
+
+        </PageHeader>
+
+        <div style={{ margin: '24px 0', padding: 0, background: '#fff'}}>
+           < Card  title = " Desposit "  style = { { marginBottom: 24 } } >
+          {content}
+          </Card>
+        </div>
+      </>
+    );
+  }
+  
+  
+}
+
+//
+export default Form.create() (withRouter(connect(
+    (state)=> ({
+        accounts:         accountsRedux.accounts(state),
+        actualAccountName:    loginRedux.actualAccountName(state),
+        actualRole:       loginRedux.actualRole(state),
+        actualPrivateKey: loginRedux.actualPrivateKey(state),
+        isLoading:        loginRedux.isLoading(state),
+        balance:          balanceRedux.userBalanceFormatted(state),
+    }),
+    (dispatch)=>({
+        
+    })
+)(processExternal) )
+);
