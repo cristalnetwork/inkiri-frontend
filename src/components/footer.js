6ebec4c97d816e6cbbb2656e9f865c584b56fa68
tuti <ptutino@gmail.com>
2020-06-18 15:11:58 -0300

    New: IUGU payments from business account. Merged with labis.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import * as globalCfg from '@app/configs/global';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import VersionIndicator from '@app/components/version_indicator';
 
 const UIFooter = (props) => {
   
+  if(props.with_version)
+  {
+    return (<>{ `CRISTAL NETWORK © ${(new Date()).getFullYear()} v ${globalCfg.version}`} <VersionIndicator /></>);
+  }
   return (
     <>
       <FontAwesomeIcon icon={['fab', 'creative-commons']} /> { ` ${(new Date()).getFullYear()} Version ${globalCfg.version}`}
